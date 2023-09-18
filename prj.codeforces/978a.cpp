f75060132adf3e0bd3d2c1107685d7b53f13a73e
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-09-18 13:57:14 +0300

    Add files via upload

---
+++ prj.codeforces/978a.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <set>
+int main() {
+    std::set <int> a;
+    int b;
+    std::cin >> b;
+    for (int i = 0; i < b; i++) {
+        int s;
+        std::cin >> s;
+        a.insert(s);
+    }
+    std::cout << a.size()<<"\n";
+    for (auto now: a) {
+        std::cout << now<<" ";
+    }
+    return 0;
+}
