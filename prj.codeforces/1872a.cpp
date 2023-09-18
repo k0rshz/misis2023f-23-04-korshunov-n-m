2a26537b66810ade54310309df8b740decac9b10
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-09-18 13:54:11 +0300

    Add files via upload

---
+++ prj.codeforces/1872a.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+int main() {
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        int k = 0;
+        double a, b, c,d;
+        std::cin >> a >> b >> c;
+        if (a < b) {
+            d = a;
+            a = b;
+            b = d;
+        }
+        while (a-c > b + c) {
+            k++;
+            b += c;
+            a -= c;
+        }
+        if (a != b) {
+            k++;
+        }
+        std::cout << k << "\n";
+    }
+    return 0;
+}
