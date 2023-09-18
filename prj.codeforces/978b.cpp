8fd43a34c65a1f32d0084a10cbe24526c5e1646e
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-09-18 13:58:25 +0300

    Add files via upload

---
+++ prj.codeforces/978b.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+int main() {
+    int b;
+    std::cin >> b;
+    std::string a;
+    std::cin >> a;
+    int k=0,c=0;
+    for (int i=0; i<b; i++) {
+        if (a[i] == 'x') {
+            k++;
+        }
+        else {
+            k = 0;
+        }
+        if ((k % 3 == 0 && k != 0) || (k>3)) {
+            c++;
+        }
+    }
+    std::cout << c;
+    return 0;
+}
