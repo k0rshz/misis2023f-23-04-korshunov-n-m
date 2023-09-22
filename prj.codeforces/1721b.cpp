21108e2894b53b9292745f2bfaacd7afc0e5e799
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-09-23 00:11:13 +0300

    Add files via upload

---
+++ prj.codeforces/1721b.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+int main() {
+    int n;
+    std::cin >> n;
+    for (int g = 0; g < n; g++) {
+        int n, m, sx, sy, d;
+        std::cin >> n >> m >> sx >> sy >> d;
+        if ((abs(sx-1)<= d && abs(sy - 1) <= d) || (abs(sx - 1) <= d && abs(sx - n) <= d) || (abs(sy - 1) <= d && abs(sy - m) <= d) || (abs(sx - n) <= d && abs(sy - m) <= d)) {
+            std::cout << -1<<"\n";
+        }
+        else {
+            std::cout << n + m - 2<<"\n";
+        }
+    }
+    return 0;
+}
