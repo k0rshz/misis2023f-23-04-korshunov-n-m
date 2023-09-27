72daab30d129995483596cbf17ec04fd24a29b56
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-09-27 19:11:08 +0300

    Add files via upload

---
+++ prj.codeforces/1295a.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+int main() {
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a,num,ost;
+        std::string s = "";
+        std::cin >> a;
+        num = a / 2;
+        ost = a % 2;
+        for (int j = 0; j < num; j++) {
+            if (ost == 1) {
+                s += "7";
+                ost -= 1;
+            }
+            else {
+                s += "1";
+            }
+        }
+        std::cout << s << "\n";
+    }
+    return 0;
+}
