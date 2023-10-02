f8b3e903b41cb7ffa62fc45171c6a903a7c64e60
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-02 10:30:41 +0300

    Add files via upload

---
+++ prj.codeforces/339b.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+int main() {
+    int n, m,s=1;
+    long long int k = 0;
+    std::cin >> n >> m;
+    std::vector <int> a;
+    for (int i = 0; i < m; i++) {
+        int b;
+        std::cin >> b;
+        a.push_back(b);
+    }
+    for (int i = 0; i < m; i++) {
+        if (a[i]-s>=0) {
+            k += a[i] - s;
+        }
+        else {
+            k += n-(s - a[i]);
+        }
+        s = a[i];
+    }
+    std::cout << k;
+    return 0;
+}
