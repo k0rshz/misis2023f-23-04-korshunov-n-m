fcd46d3cf600e0f5db27f9205adeae9e8711d986
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-03 19:14:21 +0300

    Add files via upload

---
+++ prj.codeforces/1873d.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+int main() {
+    int m;
+    std::cin >> m;
+    for (int g = 0; g < m; g++) {
+        int n, k, res = 0;
+        std::cin >> n >> k;
+        std::string s;
+        std::cin >> s;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'B') {
+                res += 1;
+                i += k - 1;
+            }
+        }
+        std::cout << res << "\n";
+    }
+    return 0;
+}
