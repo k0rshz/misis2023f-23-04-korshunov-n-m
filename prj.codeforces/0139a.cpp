00af4d251adee71a52608694552eead76d15ef7d
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-04 15:15:30 +0300

    Add files via upload

---
+++ prj.codeforces/0139a.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+int main() {
+    int n,ch=0,k=0;
+    std::cin >> n;
+    std::vector <int> a;
+    for (int i = 0; i < 7; i++) {
+        int t;
+        std::cin >> t;
+        a.push_back(t);
+    }
+    while (ch < n) {
+        if (k < 6) {
+            ch += a[k];
+            k++;
+        }
+        else {
+            ch += a[k];
+            k = 0;
+        }
+    }
+    if (k == 0) {
+        std::cout << 7;
+    }
+    else {
+        std::cout << k;
+    }
+    return 0;
+}
