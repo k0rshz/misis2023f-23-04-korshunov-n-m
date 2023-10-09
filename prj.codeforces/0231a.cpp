6aa0befd837b816ad839dd4ad7ae40ad708aed48
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-09 09:35:11 +0300

    Add files via upload

---
+++ prj.codeforces/0231a.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+int main() {
+    int n, k = 0;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+        std::cin >> a >> b >> c;
+        if (a + b + c > 1) {
+            k++;
+        }
+    }
+    std::cout << k;
+    return 0;
+}
