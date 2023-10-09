8c5aa7a2713b9b8b1503866c98aeec1c74531616
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-09 09:38:09 +0300

    Add files via upload

---
+++ prj.codeforces/0071a.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+int main() {
+    int n;
+    std::cin >> n;
+    int k = 0;
+    for (int i = 0; i < n; i++) {
+        std::string a;
+        std::cin >> a;
+        a = a + " ";
+        int k = 0;
+        while (a[k] != ' ') {
+            k++;
+        }
+        if (k > 10) {
+            std::cout << a[0] << k - 2 << a[k - 1] << "\n";
+        }
+        else {
+            std::cout << a << "\n";
+        }
+    }
+    return 0;
+}
