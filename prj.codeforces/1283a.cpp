c875397080b4509774e48975c99225ca26ef4b96
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-09 09:40:41 +0300

    Add files via upload

---
+++ prj.codeforces/1283a.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+int main() {
+    int n, c = 1440;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        int h, m;
+        std::cin >> h >> m;
+        std::cout << c - h * 60 - m << "\n";
+    }
+    return 0;
+}
