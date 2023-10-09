eaf195168c022e5b3d498e6251215e0dc37af971
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-09 09:32:49 +0300

    Add files via upload

---
+++ prj.codeforces/0263a.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+int main() {
+    int n = 1, m = 0, a, b, c;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            std::cin >> c;
+            m++;
+            if (c == 1) {
+                a = n;
+                b = m;
+            }
+        }
+        m = 0;
+        n++;
+    }
+    std::cout << abs(a - 3) + abs(b - 3);
+    return 0;
+}
