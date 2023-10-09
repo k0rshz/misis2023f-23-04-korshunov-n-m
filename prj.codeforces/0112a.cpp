f08b3560ba734a385cc126e6764891f070b50f42
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-09 09:39:26 +0300

    Add files via upload

---
+++ prj.codeforces/0112a.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+int main() {
+    std::string a, b, c = "", d = "";
+    std::cin >> a >> b;
+    int k = 0;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] >= 97 && a[i] <= 122) {
+            c += a[i] - 32;
+        }
+        else {
+            c += a[i];
+        }
+        if (b[i] >= 97 && b[i] <= 122) {
+            d += b[i] - 32;
+        }
+        else {
+            d += b[i];
+        }
+        if (c < d) {
+            k = -1;
+            break;
+        }
+        else if (c > d) {
+            k = 1;
+            break;
+        }
+    }
+    std::cout << k;
+    return 0;
+}
