14c154964d42187d0404eb375e361440965d6b4f
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-09 13:55:58 +0300

    Add files via upload

---
+++ prj.codeforces/1559a.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+int main() {
+    int m;
+    std::cin >> m;
+    for (int g = 0; g < m; g++) {
+        int n,s=0,q;
+        std::cin >> n;
+        std::vector <int> a;
+        for (int i = 0; i < n; i++) {
+            int t;
+            std::cin >> t;
+            if (t >=s) {
+                q = i;
+                s = t;
+            }
+            a.push_back(t);
+        }
+        for (int s = 0; s < 10; s++) {
+            for (int i = n - 1; i > 0; i--) {
+                for (int j = 0; j <= i; j++) {
+                    int k = i - j, z = j;
+                    while (k >= 0) {
+                        k--;
+                        int t = a[z] & a[i - z];
+                        a[z] = t;
+                        z++;
+                    }
+                }
+            }
+        }
+        std::cout << a[q] << "\n";
+    }
+    return 0;
+}
