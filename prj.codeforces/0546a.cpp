c39c1f87d4ce3924fc0de2698f9f57d78aaa6487
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-09 09:36:52 +0300

    Add files via upload

---
+++ prj.codeforces/0546a.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+int main() {
+    int w, k, n, a = 0;
+    std::cin >> k >> n >> w;
+    for (int i = 1; i <= w; i++) {
+        a += k * i;
+    }
+    if (a > n) {
+        std::cout << a - n;
+    }
+    else {
+        std::cout << 0;
+    }
+    return 0;
+}
