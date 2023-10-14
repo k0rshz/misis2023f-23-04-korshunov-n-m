cec3b6640304722e4b086aabe16fabdad07e9840
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-14 12:17:58 +0300

    Add files via upload

---
+++ prj.codeforces/0137b.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+int main() {
+    int n,k=0;
+    std::cin >> n;
+    std::vector <int> b;
+    for (int i = 0; i < n; i++) {
+        int a;
+        std::cin >> a;
+        b.push_back(a);
+    }
+    std::sort(std::begin(b), std::end(b));
+    for (int i = 1; i <= n; i++) {
+        int flag = 0;
+        for (int j = 0; j < b.size(); j++) {
+            if (b[j] == i) {
+                flag = 1;
+            }
+        }
+        if (flag != 1) {
+            k++;
+        }
+    }
+    std::cout << k;
+}
