ea0a4b8b1b14c6050d231e38233fcfb08264aebe
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-16 10:15:55 +0300

    Add files via upload

---
+++ prj.codeforces/0731a.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+int main() {
+    int k = 0;
+    std::string s;
+    std::cin >> s;
+    char w = 'a';
+    for (int i = 0; i < s.size(); i++) {
+        char t = s[i];
+        k += std::min(26 - abs(w-t), abs(w - t));
+        w = t;
+    }
+    std::cout << k;
+    return 0;
+}
