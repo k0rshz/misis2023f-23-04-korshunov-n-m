f887bd69b7e77e71cee6065b0c3b04e13d5d5f81
tox1ck1d <143878249+tox1ck1d@users.noreply.github.com>
2023-10-22 01:07:06 +0300

    Add files via upload

---
+++ prj.codeforces/1870a.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+int main() {
+    int m;
+    std::cin >> m;
+    for (int g = 0; g < m; g++) {
+        int n, k, x, c=0, f=0;
+        std::cin >> n >> k >> x;
+        if ((x + 1 < k) || (n<k)) {
+            std::cout << -1 << "\n";
+        }
+        else {
+            for (int i = 0; i < k; i++) {
+                c += i;
+                f++;
+            }
+            while (f < n) {
+                f++;
+                if (x != k) {
+                    c += x;
+                }
+                else {
+                    c += x - 1;
+                }
+            }
+            std::cout << c << "\n";
+        }
+    }
+    return 0;
+}
